bcee9cedbd08c96c94e115d165495622f20a9015
AliAbdulle <alidhobey@gmail.com>
2020-01-29 20:28:43 -0600

    Added contents

---
+++ src/Resume.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Link } from "@reach/router";
+
+class Welcome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const { userName, logOutUser } = this.props;
+    return (
+        <div className="text-center">
+      <span className="text-secondary font-weight-bold pl-1">
+          Welcome {userName}
+      </span>
+      ,
+        <Link to="/login" className="font-weight-bold text-primary pl-1"
+        onClick={e => logOutUser(e)}>
+          Logout
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
