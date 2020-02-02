c640f9a25d987eae610f6d7c9e71ed49358488b1
AliAbdulle <alidhobey@gmail.com>
2020-02-01 22:29:43 -0600

    added jumotron css color

--- src/Resume.js
+++ src/Resume.js
@@ -15,7 +15,11 @@ class Resume extends Component {
               </h1>
               <p>This meeting is provided my the company name LinkIN Came</p>
               <h2> Work Experiences</h2>
-             
+              <ul>
+                  <li>Ceve logistics</li>
+                  <li>Dell Technology</li>
+                  <li>IBM </li>
+              </ul>
             </div>
           </div>
           
